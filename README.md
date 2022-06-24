8b9c5a4e7ab7c9e093d8a9bcc746adf2a1392752
Oleksandr Zorenko <morf268@ukr.net>
2022-06-24 15:57:27 +0200

    [ADD] added requirements.txt and cors middleware

---
+++ README.md
@@ -0,0 +1,9 @@
+
+Social media api with Fast API
+
+Features of this api
+
+* CRUD operations on posts
+* JWT Token based authentication
+* Comment Posts
+* Vote/Like posts
--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app import models
 from app.database import engine
 from app.routers import post, user, auth, vote
@@ -7,6 +8,16 @@ from app.routers import post, user, auth, vote
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
